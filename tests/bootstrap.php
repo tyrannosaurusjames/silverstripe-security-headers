90f1c8888b9e589217fb43ea7ad0b89fbc324210
James Goodman <james.s.goodman@gmail.com>
2015-03-27 22:10:45 +1300

    Travis config

---
+++ tests/bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+
+$autoloadFilename = __DIR__ . '/../../vendor/autoload.php';
+$frameworkBootstrapFilename = __DIR__ . '/../../framework/tests/bootstrap.php';
+
+if (!file_exists($autoloadFilename)) {
+    echo 'You must first install the vendors using composer.' . PHP_EOL;
+    exit(1);
+}
+
+if (!file_exists($frameworkBootstrapFilename)) {
+    echo 'Can\'t find framework bootstrap.php' . PHP_EOL;
+    exit(1);
+}
+
+require $autoloadFilename;
+require $frameworkBootstrapFilename;
