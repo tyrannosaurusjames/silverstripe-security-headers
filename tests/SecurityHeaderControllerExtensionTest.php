cd5a73b75af3daaecdd96b88c9ec94340a622bfd
James Goodman <james.s.goodman@gmail.com>
2015-03-28 22:10:20 +1300

    Add a more robust test for different user agents

--- tests/SecurityHeaderControllerExtensionTest.php
+++ tests/SecurityHeaderControllerExtensionTest.php
@@ -52,4 +52,39 @@ class SecurityHeaderControllerExtensionTest extends FunctionalTest
         }
     }
 
+    public function testBrowserHasWorkingCSPImplementation()
+    {
+        $safari5Response = $this->getResponseForUserAgent(
+            'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_6; de-de) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27'
+        );
+
+        $this->assertNull($safari5Response->getHeader('Content-Security-Policy'));
+
+        $safari7Response = $this->getResponseForUserAgent(
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A'
+        );
+
+        $this->assertEquals('test-value', $safari7Response->getHeader('Content-Security-Policy'));
+
+        $chromeResponse = $this->getResponseForUserAgent(
+            'User-Agent: Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36'
+        );
+
+        $this->assertEquals('test-value', $chromeResponse->getHeader('Content-Security-Policy'));
+    }
+
+    private function getResponseForUserAgent($userAgent)
+    {
+        return Director::test(
+            'security-header-test',
+            null,
+            null,
+            null,
+            null,
+            array(
+                'User-Agent' => $userAgent
+            )
+        );
+    }
+
 }
